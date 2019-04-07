a3cea312c4b3e69ba6f3f0a5b1164512c978e1fe
Paul Lysak <paul.lysak@gmail.com>
2019-04-07 15:49:19 +0300

    Project setup

---
+++ src/test/kotlin/karazinscalausersgroup/kotlin/coroutines/SampleSpec.kt
@@ -0,0 +1,12 @@
+package karazinscalausersgroup.kotlin.coroutines
+
+import io.kotlintest.specs.StringSpec
+
+
+class SampleSpec: StringSpec() {
+    init {
+        "Sample case" {
+            //all right
+        }
+    }
+}
